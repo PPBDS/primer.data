0e46a1f42de3ea32f909ae59ab534dbb2c086757
David Kane <dave.kane@gmail.com>
2021-04-07 09:04:36 -0400

    trying to pass check

--- R/mail.R
+++ R/mail.R
@@ -1,16 +1,15 @@
-#' @title
-#' Philadelphia experiment on mail-in voting
+#' @title Philadelphia experiment on mail-in voting
 #'
 #' @description
 #'
-#' This data is from a paper entitled \href{Results from a 2020 field experiment
-#' encouraging voting by
-#' mail}{https://github.com/PPBDS/primer.data/blob/master/inst/papers/mail.pdf}.
-#' See also the 'Details' section below. The aim of the study was to understand
-#' how facilitating mail ballot requests affects their use. A total of over
-#' 935,000 registered voters in Philadelphia were randomly assigned to either a
-#' control group or one of two treatment groups. The results combine information
-#' on voting behavior with voter's demographic background.
+#' This data is from a paper entitled
+#' \href{https://github.com/PPBDS/primer.data/blob/master/inst/papers/mail.pdf}{Results
+#' from a 2020 field experiment encouraging voting by mail}. See also the
+#' 'Details' section below. The aim of the study was to understand how
+#' facilitating mail ballot requests affects their use. A total of over 935,000
+#' registered voters in Philadelphia were randomly assigned to either a control
+#' group or one of two treatment groups. The results combine information on
+#' voting behavior with voter's demographic background.
 #'
 #'
 #' @details
@@ -33,30 +32,26 @@
 #'
 #' &nbsp;
 #'
-#' ```{r, echo = FALSE}
-#' skimr::skim(mail)
-#' ```
+#' ```{r, echo = FALSE} skimr::skim(mail) ```
 #'
-#' @format A tibble with 935,707 observations and 11 variables:
-#' \describe{
-#'   \item{treatment}{character variable indicating which of the 3 treatments was
-#'   employed before the 2020 primary election: 'No Postcard', 'Self', or 'Neighborhood'}
-#'   \item{voted}{character variable for whether respondent voted}
-#'   \item{voted_mail}{character variable for whether respondent voted by mail}
-#'   \item{applied_mail}{character variable for whether respondent applied for a mail ballot}
-#'   \item{applied_date}{date variable for the date that respondent's mail ballot application was received}
-#'   \item{voted_date}{date variable for the date that respondent's mail ballot was received}
+#' @format A tibble with 935,707 observations and 11 variables: \describe{
+#'   \item{treatment}{character variable indicating which of the 3 treatments
+#'   was employed before the 2020 primary election: 'No Postcard', 'Self', or
+#'   'Neighborhood'} \item{voted}{character variable for whether respondent
+#'   voted} \item{voted_mail}{character variable for whether respondent voted by
+#'   mail} \item{applied_mail}{character variable for whether respondent applied
+#'   for a mail ballot} \item{applied_date}{date variable for the date that
+#'   respondent's mail ballot application was received} \item{voted_date}{date
+#'   variable for the date that respondent's mail ballot was received}
 #'   \item{party}{character variable for respondent's party registration}
-#'   \item{age}{factor variable for respondent's age group}
-#'   \item{sex}{character variable for respondent's sex}
-#'   \item{pred_white}{double variable for likelihood of respondent being white based on their name}
-#'   \item{pred_black}{double variable for likelihood of respondent being black based on their name}
-#' }
+#'   \item{age}{factor variable for respondent's age group} \item{sex}{character
+#'   variable for respondent's sex} \item{pred_white}{double variable for
+#'   likelihood of respondent being white based on their name}
+#'   \item{pred_black}{double variable for likelihood of respondent being black
+#'   based on their name} }
 #'
-#' @author
-#' David Kane
+#' @author David Kane
 #'
-#' @source
-#' \url{https://doi.org/10.7910/DVN/HUUEGI}
+#' @source \url{https://doi.org/10.7910/DVN/HUUEGI}
 #'
 "mail"
