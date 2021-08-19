aaad7671a88c3e7b817047ba5820cb1719331264
Thomas Weiss <tcweiss@protonmail.com>
2021-08-19 20:01:37 +0200

    'shootings' improvements (#29)
    * added shootings documentation & API
    
    * added trump data
    
    * added deco data

---
+++ R/shootings.R
@@ -0,0 +1,31 @@
+#' @title People shot by US police (2017-)
+#'
+#' @description
+#'
+#' This data comes from the book \href{https://crimebythenumbers.com}{Crime by
+#' the Numbers: A Criminologist’s Guide to R}, which compiles multiple data sets from
+#' the FBI Uniform Crime Report. Variables included here contain information
+#' on shooting victims of US police between 2015 and 2019.
+#'
+#' @format A tibble with 4371 observations and 13 variables:
+#' \describe{
+#'   \item{name}{character variable for name of shooting victim}
+#'   \item{date}{date variable for date of shooting}
+#'   \item{state}{character variable for state in which shooting took place}
+#'   \item{city}{character variable for city in which shooting took place}
+#'   \item{manner_of_death}{character variable for manner of death}
+#'   \item{weapon}{character variable for weapon used by shooting victim}
+#'   \item{age}{integer variable for age of shooting victim}
+#'   \item{gender}{character variable for gender of shooting victim}
+#'   \item{race}{character variable for race of shootings victim}
+#'   \item{mental_illness_signs}{character variable indicating whether shooting victim showed signs of mental illness}
+#'   \item{threat_level}{character level for tyoe of threat posed by shooting victim}
+#'   \item{flee}{character variable indicating whether shooting victim tried to flee}
+#'   \item{body_camera}{character variable indicating whether police officer involved carried a body camera}
+#'   }
+#'
+#' @author David Kane
+#'
+#' @source \url{https://github.com/jacobkap/crimebythenumbers/blob/master/data/shootings.csv}
+#'
+"shootings"
--- data-raw/make_shootings.R
+++ data-raw/make_shootings.R
@@ -1,5 +1,4 @@
 
-
 library(tidyverse)
 library(magrittr)
 library(janitor)
@@ -35,8 +34,8 @@ x <- read_csv("data-raw/shootings.csv") %>%
                                 race == "N" ~ "Other", # Not totally sure about this one. It seems that these abbreviations come from OMB, but their guidelines are confusing.
                                 race == "O" ~ "Unknown",
                                 race == "W" ~ "White"),
-               mental_illness_signs = if_else(mental_illness_signs == "TRUE", 1, 0),
-               body_camera = as.integer(body_camera))
+               mental_illness_signs = if_else(mental_illness_signs == TRUE, "Yes", "No"),
+               body_camera = if_else(body_camera == TRUE, "Yes", "No"))
 
 
 # Full state names instead of abbreviations.
@@ -52,9 +51,71 @@ x %<>%
   rename(state = full_state)
 
 
+# Reorder variables and rows.
+
+x %<>%
+  select(name, date, state, city, manner_of_death:race, everything()) %>%
+  arrange(date, state, city, name)
+
+
+
+
 # Save
 
 shootings <- x
 
 usethis::use_data(shootings, overwrite = T)
+
+
+##############################################
+#  Working on trying to access the FBI API   #
+##############################################
+#
+# library(fbicrime)
+# library(tidyverse)
+# library(magrittr)
+# library(janitor)
+#
+# # I am using the fbicrime package for accessing their API. It only returns
+# # aggregated data, but that may also be interesting. For details, check out
+# # https://medium.com/@wenjun.sarah.sun/an-r-wrapper-for-fbi-crime-api-a80f8586e0ffSet.
+#
+#
+# # Step 1: Set an API key. Get one here: https://api.data.gov/signup/.
+#
+# set_fbi_crime_api_key('oW4EOrRT8o3WCvsFpREVRgpkcllcF0IqGarjmmo5')
+#
+#
+# # Step 2: Get the data. The four argument must be used. For information on
+# # possible inputs, see https://crime-data-explorer.app.cloud.gov/pages/docApi.
+# # You can input multiple offenses at a time as a list, but only one variable. It
+# # will likely return some errors if for some group there is no data available;
+# # the data that exists will still be returned.
+#
+# state_race <- summarize_offender(offense = c('homicide',
+#                                              'rape',
+#                                              'arson'),
+#                    level = 'states',
+#                    level_detail = c('AL',	'AK',	'AZ',	'AR',	'CA',	'CO',
+#                                     'CT',	'DE',	'FL',	'GA',	'HI',	'ID',
+#                                     'IL',	'IN',	'IA',	'KS',	'KY',	'LA',
+#                                     'ME',	'MD',	'MA',	'MI',	'MN',	'MS',
+#                                     'MO',	'MT',	'NE',	'NV',	'NH',	'NJ',
+#                                     'NM',	'NY',	'NC',	'ND',	'OH',	'OK',
+#                                     'OR',	'PA',	'RI',	'SC',	'SD',	'TN',
+#                                     'TX',	'UT',	'VT',	'VA',	'WA',	'WV',
+#                                     'WI',	'WY'),
+#                    variable = 'race') %>%
+#                    tibble() %>%
+#                    clean_names() %>%
+#                    mutate(race = unlist(key)) %>%
+#                    rename(state = level_detail,
+#                           offense = type) %>%
+#                    select(year, state, offense, race, count)
+#
+# # Save this one. May create new data set from this.
+#
+# write_rds(state_race, "data-raw/state_race.rds")
+#
+# ##############################################
 
