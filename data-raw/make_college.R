91eb5a880f0f6c2ad908916263c4b59fdfe73553
Tanay Janmanchi <janmanchit@gmail.com>
2024-08-05 10:26:53 -0400

    added comments (#43)

--- data-raw/make_college.R
+++ data-raw/make_college.R
@@ -1,11 +1,24 @@
 library(tidyverse)
 
-# add comments
-# college/colleges
+# add comments. Added a few from out convo today and yesterday
+# college/colleges: going with college
 # the .Rd
 
 
+# Zip code may not be necessary, since we already have 2 other location variables, county and state.
+# It seems as if there are probably not many colleges per county, so removing zip might be a good idea.
 
+# There doesnt seem to be much of a relationship between grad_rate and tuition for
+# the schools which charge less than 20k per year, will filter them out during the EDA
+
+# Historically Black Colleges overall had an impact on graduation rate,
+# however, once we filter them out to ones where tuition is 20k+, the number of hbcu's goes fom 40 -> 7
+
+# Selectivity seems to affect graduation rate a ton
+
+# Will show a basic scatterplot in the EDA where tuition is on x-axis and grad_rate is on y
+
+# One college has a graduation rate of 3%, Bacone College which dissolved in 2018. Is it worth keeping it in our data?
 
 x <- read_csv("data-raw/mrc_table10.csv") |>
 
@@ -24,6 +37,8 @@ x <- read_csv("data-raw/mrc_table10.csv") |>
          grad_rate_150_p_2013,
          sat_avg_2013) |>
 
+  # Removing 'special' colleges since there are only 5 of them
+
   filter(barrons != 9) |>
 
   # Renaming some variables
@@ -39,7 +54,7 @@ x <- read_csv("data-raw/mrc_table10.csv") |>
   # Editing selectivity, region and type to display text instead of numbers
 
 
-
+  # selectivity is just tier, tier might not even be necessary since we are turning it into selectivity
   mutate(selectivity = case_when(tier == 1 ~ "Elite",
                                  tier == 2 ~ "Highly Selective",
                                  tier == 3 ~ "Moderately Selective",
@@ -47,12 +62,16 @@ x <- read_csv("data-raw/mrc_table10.csv") |>
                                  tier == 5 ~ "Lowly Selective",
                                  tier == 999 ~ "Non-selective")) |>
 
+  # Changing tier fom 999 to 6 so that it looks better when ordered
+
   mutate(tier = case_when(tier == 1 ~ 1,
                           tier == 2 ~ 2,
                           tier == 3 ~ 3,
                           tier == 4 ~ 4,
                           tier == 5 ~ 5,
                           tier == 999 ~ 6)) |>
+
+  # easier to interpret
 
   mutate(region = case_when(region == 1 ~ "Northeast",
                             region == 2 ~ "Midwest",
@@ -66,16 +85,21 @@ x <- read_csv("data-raw/mrc_table10.csv") |>
                             public == 2 ~ FALSE,
                             public == 3 ~ FALSE)) |>
 
-  # Turning `hbcu` into a binary variable. Keep it.
+  # Turning `hbcu` into a true/false variable.
 
   mutate(hbcu = case_when(hbcu == 0 ~ FALSE,
                           hbcu == 1 ~ TRUE)) |>
 
-  # Keep cost by change to /10000
+  # Keep cost by change to / 10000
 
   mutate(tuition = tuition / 10000) |>
 
   drop_na()
+
+
+# Ordering factors for selectivity
+# This being an ordered factor makes it harder to interpret the regression coefficients,
+# might have to make it a normal factor
 
 x$selectivity <- factor(x$selectivity,
                         levels = c("Elite",
@@ -84,19 +108,23 @@ x$selectivity <- factor(x$selectivity,
                                    "Lowly Selective",
                                    "Non-selective"),
                         ordered = TRUE)
+
+# Turning these into integers
 x$zip <- as.integer(x$zip)
 x$tier <- as.integer(x$tier)
 x$enrollment <- as.integer(x$enrollment)
+x$sat <- as.integer(x$sat)
+
+# Turning region into a factor
+
 x$region <- factor(x$region,
                    levels = c("Midwest",
                               "South",
                               "West",
                               "Northeast"))
-x$sat <- as.integer(x$sat)
 
 # Save.
 
 college <- x
-
 usethis::use_data(college, overwrite = TRUE)
 
