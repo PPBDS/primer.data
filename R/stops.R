0818f1438e2696a80f2221b86d80fe8e2054fd30
SanakaDash <sanakadash@gmail.com>
2024-08-05 22:47:47 -0400

    Fixed Errors - "Stops"

--- R/stops.R
+++ R/stops.R
@@ -2,21 +2,26 @@
 #' New Orleans Traffic Stops Data
 #'
 #' @description
-#' This data is from the \href{https://openpolicing.stanford.edu/findings/}{Stanford Open Policing Project},
-#' which aims to improve police accountability and transparency by providing data on traffic stops across
-#' the United States. The \href{https://openpolicing.stanford.edu/data/#new-orleans}{New Orleans dataset}
-#' includes detailed information about traffic stops conducted by the New Orleans Police Department.
+#' This data is from the \href{https://openpolicing.stanford.edu/findings/}
+#' {Stanford Open Policing Project}, which aims to improve police accountability
+#' and transparency by providing data on traffic stops across the United
+#' States. The \href{https://openpolicing.stanford.edu/data/#new-orleans}
+#' {New Orleans dataset} includes detailed information about traffic stops
+#' conducted by the New Orleans Police Department.
 #'
 #' @details
-#' The dataset includes information about the date, time, and location of each stop, as well as demographic
-#' details about the driver and the outcomes of the stop. The data covers traffic stops from 2011 to 2017.
-#' The purpose of this dataset is to facilitate research on racial disparities in policing practices.
+#' The dataset includes information about the date, time, and location of each
+#' stop, as well as demographic details about the driver and the outcomes of
+#' the stop. The data covers traffic stops from July 1, 2011 to July 18, 2018.
+#' The purpose of this dataset is to facilitate research on racial disparities
+#' in policing practices.
 #'
 #' @format A tibble with 1,000,000+ observations and 15 variables:
 #' \describe{
 #' \item{date}{date variable indicating the date of the stop}
 #' \item{time}{time variable indicating the time of the stop}
-#' \item{zone}{character variable indicating the zone of the officer conducting the stop}
+#' \item{zone}{character variable indicating the zone of the officer conducting
+#' the stop}
 #' \item{race}{character variable indicating the race of the driver}
 #' \item{sex}{character variable indicating the sex of the driver}
 #' \item{age}{integer variable indicating the age of the driver}
--- data-raw/make_stops.R
+++ data-raw/make_stops.R
@@ -1,67 +1,64 @@
 library(tidyverse)
-library(brms)
 
-# read through all the other make_*.R files
-# select the variables to keep
-# select the rows to keep: arrest, sex, race, age, district, location, time
-# SD: I felt that instead of both district & location, it would be better to hav the Police Zone
-# rename variables
-# transform the variables
+# delete any zones with only 1 observation - DONE
 
-x <- read_csv("data-raw/yg821jf8611_la_new_orleans_2020_04_01.csv.zip",
-              guess_max = 100000, # needed b/c the default guesses the wrong coltypes, so it needs a bigger guess size.
-              col_types = cols(date = col_date(format = ""), # still giving coltypes for the variable that we're using.
-                               time = col_time(),
-                               zone = col_character(),
-                               subject_age = col_integer(),
-                               subject_race = col_character(),
-                               subject_sex = col_character(),
-                               arrest_made = col_logical())) |>
+# Drop the NAs since we don't want to bother students with this complexity.
+# Note that this costs about 10,000 observations (the vast majority because of
+# missing age) out of 400,000. It is a deep question whether or not we should
+# drop these rows.
+# SD: I say we keep the NAs b/c if we're trying to get an insight on the percent
+# of people getting arrested, it would be useful to have that data.
 
-# Selecting the variables specified at the top.
+# fix .R file, and then regenerate the help page - DONE
 
-select(date, time, zone, # I wanted to keep date and time separate b/c something can be done with just the time.
-       subject_age, subject_race,
-       subject_sex, arrest_made) |>
+# Using a big guess_max because there are some weird values deep in the data.
 
-# Renaming some variables so they're easier to use.
+raw <- read_csv("data-raw/yg821jf8611_la_new_orleans_2020_04_01.csv.zip",
+                guess_max = 100000,
+                col_types = cols(date = col_date(format = ""),
+                                 time = col_time(),
+                                 zone = col_character(),
+                                 subject_age = col_integer(),
+                                 subject_race = col_character(),
+                                 subject_sex = col_character(),
+                                 arrest_made = col_logical()))
 
-rename(age = "subject_age",
-       sex = "subject_sex",
-       race = "subject_race",
-       arrested = "arrest_made") |>
+# I wanted to keep date and time separate b/c something can be done with just
+# the time.
+# I felt that instead of both district & location, it would be better to have
+# the Police Zone instead.
 
-# Drop all zones without more than 1 observation.
+x <- raw |>
+  select(date, time, zone,
+         subject_age, subject_race,
+         subject_sex, arrest_made) |>
 
-distinct()
+  rename(age = "subject_age",
+         sex = "subject_sex",
+         race = "subject_race",
+         arrested = "arrest_made") |>
 
-# Checking whether any row includes the same value for every variable.
+  # There were 4 rows with NA dates. We get rid of them but we really ought to
+  # check if any dates are missing.
+  # Note that there were 0 NAs in the `arrested` column, so it made sense to
+  # keep the NAs universally.
 
-df <- x
-df$unique <- !(duplicated(x) | duplicated(x, fromLast = TRUE))
-df <- df |>
-  filter(unique == FALSE)
-stopifnot(nrow(df) == 0)
+  drop_na(date) |>
+
+  # Seems like there are almost no arrests for the first 18 months of the data.
+  # By July 1, 2011, however, the value of arrested seems reasonable.
+
+  filter(date >= "2011-07-01") |>
+
+  # Delete any zones with only 1 observation - this was the easiest way I
+  # found, but is there any better way?
+
+  group_by(zone) %>%
+  filter(n() > 1) %>%
+  ungroup()
 
 # Save.
 
 stops <- x
 
 usethis::use_data(stops, overwrite = TRUE)
-
-
-# ------------------
-
-# Using this code makes for an interesting time series:
-
-# x |> count(date) |> arrange(n) |> ggplot(aes(x = date, y = n)) + geom_point()
-
-# Not sure what to do with this?
-
-# This makes clear why we need to delete the first 18 months of data.
-
-#  x |> select(arrest_made, date) |> mutate(all = sum(arrest_made), .by = date) |> ggplot(aes(x = date, y = all)) + geom_point()
-
-# SD: This would be better done in a tutorial rather than here
-
-# ------------------
