2d493fcc945582a671d110eceebe28e4dfe1e37a
Stepher Sabio <stepher.sabio01@gmail.com>
2025-08-18 17:04:16 +0800

    added a dataset called polls

---
+++ data-raw/make_polls.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+raw <- read_csv("data-raw/elections-poll-flsen-3.csv",
+              col_types = cols(
+                 response = col_character(),
+                 educ = col_character(),
+                 race_eth = col_character(),
+                 gender = col_character(),
+                 approve = col_character(),
+                 region = col_character(),
+                 turnout_score = col_number(),
+                 final_weight = col_number()))
+
+x <- raw |>
+  select(response, educ, race_eth, gender, approve, region, turnout_score, final_weight)|>
+  rename(
+         education = "educ",
+         race = "race_eth"
+  )
+
+
+polls <- x
+
+usethis::use_data(polls, overwrite = TRUE)
